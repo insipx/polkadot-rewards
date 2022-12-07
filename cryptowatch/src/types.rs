d53a1b56a39b0cf720ea005b52e36f646198b4de
insipx <aplaza@liquidthink.net>
2022-12-06 23:03:31 -0500

    types

--- cryptowatch/src/types.rs
+++ cryptowatch/src/types.rs
@@ -1,14 +1,30 @@
-use chrono::naive::NaiveDateTime;
-use std::borrow::Cow;
+use chrono::{serde::ts_seconds, DateTime, Utc};
+use std::{borrow::Cow, collections::HashMap};
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Response<T> {
 	result: T,
+	allowance: ApiAllowance<'static>,
 }
 
-#[derive(Serialize, Deserialize)]
+impl<T> Response<T> {
+	/// unpack the `[Response]` yeilding its associated body, `T`.
+	pub fn unpack(self) -> T {
+		self.result
+	}
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ApiAllowance<'a> {
+	cost: f64,
+	remaining: f64,
+	upgrade: Cow<'a, str>,
+}
+
+/// An single asset
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Asset<'a> {
 	id: u64,
 	#[serde(borrow)]
@@ -22,7 +38,8 @@ pub struct Asset<'a> {
 	route: Cow<'a, str>,
 }
 
-#[derive(Serialize, Deserialize)]
+/// An asset/asset pairing.
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Pair<'a> {
 	id: u64,
 	#[serde(borrow)]
@@ -35,31 +52,86 @@ pub struct Pair<'a> {
 	route: Cow<'a, str>,
 }
 
-#[derive(Serialize, Deserialize)]
-pub enum Period<T> {
-	P60(T),
-	P180(T),
-	P300(T),
-	P900(T),
-	P1800(T),
-	P3600(T),
-	P7200(T),
-	P14400(T),
-	P21600(T),
-	P43200(T),
-	P86400(T),
-	P259200(T),
-	P604800(T),
-	P604800Monday(T),
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(transparent)]
+pub struct PeriodMap(HashMap<Period, Vec<OHLC>>);
+
+/// The candle length ("Period") in seconds. Ranges from 1 minute to 1 week.
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Hash)]
+pub enum Period {
+	#[serde(rename = "60")]
+	P60,
+	#[serde(rename = "180")]
+	P180,
+	#[serde(rename = "300")]
+	P300,
+	#[serde(rename = "900")]
+	P900,
+	#[serde(rename = "1800")]
+	P1800,
+	#[serde(rename = "3600")]
+	P3600,
+	#[serde(rename = "7200")]
+	P7200,
+	#[serde(rename = "14400")]
+	P14400,
+	#[serde(rename = "21600")]
+	P21600,
+	#[serde(rename = "43200")]
+	P43200,
+	#[serde(rename = "86400")]
+	P86400,
+	#[serde(rename = "259200")]
+	P259200,
+	#[serde(rename = "604800")]
+	P604800,
+	#[serde(rename = "604800_Monday")]
+	P604800Monday,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct OHLC {
-	close_time: NaiveDateTime,
+	#[serde(with = "ts_seconds")]
+	close_time: DateTime<Utc>,
 	open_price: f64,
 	high_price: f64,
 	low_price: f64,
 	close_price: f64,
 	volume: f64,
 	quote_volume: f64,
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use std::path::PathBuf;
+
+	// TODO: Return an impl reader and accept an enum for type of response.
+	fn load_test_data() -> (Vec<u8>, Vec<u8>, Vec<u8>) {
+		let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+		let (mut assets, mut pairs, mut ohlc) = (manifest_dir.clone(), manifest_dir.clone(), manifest_dir.clone());
+		assets.push("data/assets.json");
+		pairs.push("data/pairs.json");
+		ohlc.push("data/ohlc.json");
+
+		(std::fs::read(assets).unwrap(), std::fs::read(pairs).unwrap(), std::fs::read(ohlc).unwrap())
+	}
+
+	#[test]
+	fn test_asset_deserialization() {
+		let (assets, _, _) = load_test_data();
+		let _: Response<Vec<Asset>> = serde_json::from_slice(assets.as_slice()).unwrap();
+	}
+
+	#[test]
+	fn test_pair_deserialization() {
+		let (_, pairs, _) = load_test_data();
+		let _: Response<Vec<Pair>> = serde_json::from_slice(pairs.as_slice()).unwrap();
+	}
+
+	#[test]
+	fn test_ohlc_deserialization() {
+		let (_, _, ohlc) = load_test_data();
+		let _: Response<PeriodMap> = serde_json::from_slice(ohlc.as_slice()).unwrap();
+	}
 }
